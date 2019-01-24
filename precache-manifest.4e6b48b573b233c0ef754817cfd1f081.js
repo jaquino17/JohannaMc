60a2a360e826035c39f55dcdc189be792b74bd82
G9temp4 <g9temp4@G9temp-C02RX7HEFVH7.local>
2019-01-24 12:28:32 -0500

    Updates

---
+++ precache-manifest.4e6b48b573b233c0ef754817cfd1f081.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e8580202ea279f6f376",
+    "url": "/johannaMC/static/js/runtime~main.1e858020.js"
+  },
+  {
+    "revision": "bf7c69005aba00d747b8",
+    "url": "/johannaMC/static/js/main.bf7c6900.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/johannaMC/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "bf7c69005aba00d747b8",
+    "url": "/johannaMC/static/css/main.3278fcd4.chunk.css"
+  },
+  {
+    "revision": "a0baad42f79c82daeac15f495eb826b1",
+    "url": "/johannaMC/index.html"
+  }
+];
